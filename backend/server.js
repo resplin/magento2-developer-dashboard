7fd8e454376469c30397e3c0919db730f73b3c10
Randy Esplin <randy.esplin@ecinternet.com>
2026-04-04 13:45:36 -0700

    backend - server.js - Drop unneeded connectWithRety which referenced sequelize

--- backend/server.js
+++ backend/server.js
@@ -21,34 +21,8 @@ app.get("/", (req, res) => {
     res.json({ message: "Hello from the backend!" });
 });
 
-const connectWithRetry = async (retries = 5, delay = 3000) => {
-    for (let i = 0; i < retries; i++) {
-        try {
-            // authenticate() is a lightweight method to check if the connection is established
-            //await sequelize.authenticate();
-            //console.log('Database connected successfully.');
-
-            // Sync the database
-            //await sequelize.sync();
-            //console.log('Database synced successfully.');
-
-            return;
-        } catch (error) {
-            console.error(`Database connection failed. Attempt ${i + 1} of ${retries}. Retrying in ${delay} ms...`, error);
-
-            if (i < retries - 1) {
-                await new Promise(resolve => setTimeout(resolve, delay));
-            } else {
-                throw new Error('Failed to connect to the database after multiple attempts.');
-            }
-        }
-    }
-};
-
-// Attempt to start the server, initializing the database connection first
+// Attempt to start the server
 try {
-    await connectWithRetry();
-
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 } catch (error) {
     console.error('Unable to connect to the database:', error);
